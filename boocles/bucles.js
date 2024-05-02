4b60d08b4dca40d2d9c278498711e3a64f9e807c
felipe russi <pipelopezrussi@gmail.com>
2024-05-02 02:09:50 -0500

    "v2"

---
+++ boocles/bucles.js
@@ -0,0 +1,112 @@
+/**eje1 */
+
+// let numero = parseInt(prompt("numero?"))
+
+// for (let i = 1; i < 11; i++) {
+//     const multi = numero*i;
+//     console.log(multi);
+    
+// }
+
+/**eje 2 */
+
+// let entrada= 1
+// let suma=0
+
+// while (entrada != 0) {
+
+//     entrada= parseInt(prompt("diga un numero"))
+//     suma= suma+ entrada
+//     console.log(suma); 
+// }
+/**eje 3 */
+// let adivine= parseInt(prompt("numero"))
+// let numero= Math.round((Math.random())*100)
+// let intentos=0
+// console.log(numero);
+// while (adivine!= numero) {
+//     if (numero>adivine){
+//         console.log("el numero ingresado es menor");
+//     }
+//     else{
+//         console.log("el numero ingresado es mayor");
+//     }
+//     adivine= parseInt(prompt("numero"+numero)) 
+//     intentos++
+// }
+// console.log("ganoooooo en "+ intentos + " intentos");
+
+/**eje 4 */
+// let numero= parseInt(prompt("numero"))
+// let operacion=0
+// let  conta=0
+// let primo="es primo"
+// for (let index = 0; index < numero+1; index++) {
+//     if (numero% index=== 0){
+//         conta++
+//         if (conta>2){
+//             primo="no es primo"
+//             break
+//         }
+
+//     }    
+// }
+// console.log(primo);
+
+/**eje 5 */
+// let numero= parseInt(prompt("numero"))
+
+// for (let index = 0; index < numero+1; index++) {
+//         if (numero% index=== 0){
+//             console.log(index);
+//         }    
+//     }
+
+
+/**eje 6 */
+
+// let miArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (const iterator of miArray) {
+//     console.log(iterator);
+    
+// }
+
+/**eje 7 */
+
+// let miArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (const iterator of miArray) {
+//     console.log(iterator*2);
+    
+// }
+
+/**eje8 */
+
+// let familia = [
+//     { nombre: "Juan", edad: 35, relacion: "padre", ocupacion: "ingeniero" },
+//     { nombre: "María", edad: 33, relacion: "madre", ocupacion: "doctora" },
+//     { nombre: "Pedro", edad: 10, relacion: "hijo", ocupacion: "estudiante" },
+//     { nombre: "Ana", edad: 8, relacion: "hija", ocupacion: "estudiante" },
+//     { nombre: "Abuela", edad: 70, relacion: "abuela", ocupacion: "jubilada" }
+//   ];
+
+// for (const iterator of familia) {
+//     console.log(`hola mi nombre es ${iterator.nombre} tengo una edad de 
+//     ${iterator.edad} en la familia mi rol es ${iterator.relacion} y mi ocupacion es ser
+//     ${iterator.ocupacion}`);
+    
+// }
+
+/**eje 9 */
+// let miArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (const iterator of miArray) {
+//     if (iterator%2 != 0){
+//         console.log(iterator);
+
+//     }
+// }
+
+/**eje 10 */
+
